62ea60214636880cdf5a227f7eb149ead6d17c59
gar19085 <68084823+gar19085@users.noreply.github.com>
2021-11-22 00:07:33 -0600

    Proyecto V.5

--- ProyectoFinal/Clin.c
+++ ProyectoFinal/Clin.c
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
 */
 	RTU.sin_family = AF_INET;		// symbol constant for Internet domain
 	RTU.sin_port = htons(atoi(argv[1]));	 // port number
-	RTU.sin_addr.s_addr = htonl(INADDR_ANY); // para recibir de cualquier interfaz de red
+	//RTU.sin_addr.s_addr = htonl(INADDR_ANY); // para recibir de cualquier interfaz de red
 
 	length = sizeof(struct sockaddr_in);		 // size of structure
 
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 	if(setsockopt(sockfd1, SOL_SOCKET, SO_BROADCAST, &boolval, sizeof(boolval)) < 0)
 		error("Error setting socket options\n");
 
-
+	printf("Los comandos son los siguientes:\n");
 
     while(1){
         memset(buffer, 0, MSG_SIZE);
@@ -72,6 +72,8 @@ int main(int argc, char *argv[])
 			error("recvfrom");
 		printf("Received from RTU1: %s\n", buffer);
 
+
+		memset(buffer, 0, MSG_SIZE);
 		n = recvfrom(sockfd1, buffer, MSG_SIZE, 0, (struct sockaddr *)&from1, &length);
 		if(n < 0)
 			error("recvfrom");
@@ -83,27 +85,26 @@ void enviar(void*ptr){
 
     while(1){
 		memset(buffer, 0, MSG_SIZE);	// sets all values to zero
-        printf("Los comandos son los siguientes:\n");
-		printf("Please enter the message (! to exit): ");
+		printf("Please enter the message (! to exit): \n");
 		fgets(buffer,MSG_SIZE-1,stdin); // MSG_SIZE-1 because a null character is added        
 		if(buffer[0] != '!')
 		{
 			if((strcmp(buffer, "RTU1\n"))==0){	
-    		// Cambiamos a la dirección de broadcast
-			RTU.sin_addr.s_addr = inet_addr("192.168.0.255");	// broadcast address
-			// send message to anyone out there...
-			n = sendto(sockfd1, buffer, strlen(buffer), 0,
+    			// Cambiamos a la dirección de broadcast
+				RTU.sin_addr.s_addr = inet_addr("192.168.0.255");	// broadcast address
+				// send message to anyone out there...
+				n = sendto(sockfd1, buffer, strlen(buffer), 0,
 					  (const struct sockaddr *)&RTU, length);
-			if(n < 0)
-				error("Sendto");
+				if(n < 0)
+					error("Sendto");
 			}
-			if((strcmp(buffer, "RTU2\n"))==0){
-			RTU.sin_addr.s_addr = inet_addr("192.168.1.255");	
+				if((strcmp(buffer, "RTU2\n"))==0){
+				RTU.sin_addr.s_addr = inet_addr("192.168.1.255");	
 			
-			n = sendto(sockfd1, buffer, strlen(buffer), 0,
+				n = sendto(sockfd1, buffer, strlen(buffer), 0,
 					  (const struct sockaddr *)&RTU, length);
-			if(n < 0)
-				error("Sendto");			
+				if(n < 0)
+					error("Sendto");			
 			}
         }
     
